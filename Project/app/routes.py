eece039f2f20b20734b91d88cbc64cd7c75df2e3
Antony Li <antony.li@sjsu.edu>
2022-12-06 15:59:55 -0800

    added delete account function

--- Project/app/routes.py
+++ Project/app/routes.py
@@ -1,5 +1,5 @@
-from app import myapp_obj
-from flask import render_template, redirect, flash
+from app import myapp_obj, db
+from flask import render_template, redirect, url_for, flash
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -7,3 +7,53 @@ from flask_login import current_user
 from flask_login import login_required
 from flask_login import login_user
 from flask_login import logout_user
+
+@myapp_obj.route('/')
+def home():
+    return render_template('home.html')
+
+
+@myapp_obj.route('/login', methods=['GET', 'POST'])           #when a user logs in it checks with the database if the user exists
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        current_user = User.query.filter_by(username=form.username.data).first()
+        if current_user:
+            if check_password_hash(current_user.password, form.password.data):
+                login_user(current_user, remember=form.remember_me.data)
+                return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+
+@myapp_obj.route('/dashboard', methods=['GET', 'POST'])              
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@myapp_obj.route('/logout', methods=['GET', 'POST'])                 #logout form
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@myapp_obj.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@myapp_obj.route('/delete', methods=['GET', 'POST'])
+@login_required
+def delete():
+    current_user.remove()
+    db.session.commit()
+    flash("Account has been deleted.")
+    return redirect()
