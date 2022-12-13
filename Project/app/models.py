76ead167cda2a4b44a078d08003a8dc4d9a0c802
Ngocmai139 <117431455+Ngocmai139@users.noreply.github.com>
2022-12-12 23:08:35 -0800

    conflict update

--- Project/app/models.py
+++ Project/app/models.py
@@ -1,53 +1,40 @@
-from app import myapp_obj, db
-from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
 from flask_login import UserMixin
-import os
-from datetime import datetime
-from sqlalchemy.sql import func
-from . import db
-
-
-
-
 #to create the followers table based on the follower's ID and user's ID
 followers = db.Table('followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String, unique=True)
-    password = db.Column(db.String(200))
-    email = db.Column(db.String(32), unique=True)
-    followed = db.relationship('User',  #this is to link user to another user's account. This is a parent class
-        secondary=followers,
-        primaryjoin=(followers.c.follower_id == id),   #this is to link to the table with the condition is the follower's ID
-        secondaryjoin=(followers.c.followed_id == id), #to configured the table with the condition is the follow's ID
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic') #to define the relationship start at follower to mode dynamic
-    #to let user can follower another users 
-    #it will presendted under the table which user an see who user currently follow
-    #this is to show that message is sent by the user
-    messages_sent = db.relationship('Message',
-                                    foreign_keys='Message.sender_id',
-                                    backref='author', lazy='dynamic')
-    #this is to show the message is received by followed
-    messages_received = db.relationship('Message',
-                                        foreign_keys='Message.recipient_id',
-                                        backref='recipient', lazy='dynamic')
-    def set_password(self, password):
+class User(db.Model, UserMixin):                                                # User class model
+    id = db.Column(db.Integer, primary_key = True)                              # User ID
+    username = db.Column(db.String, unique=True)                                # Username
+    password = db.Column(db.String(200))                                        # password         
+    posts = db.relationship('Post', backref='author', lazy='dynamic')           # establishing relationship between user and their posts
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
+
+    def set_password(self, password):                                           # set password function
         self.password = generate_password_hash(password)
 
-    def check_password(self, password):
+    def check_password(self, password):                                         # check password function
         return check_password_hash(self.password, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
-    def remove(self):
+    def remove(self):                                                           # remove/delete account function
         db.session.delete(self)
+        
+    def my_posts(self):
+        own = Post.query.filter_by(user_id=self.id)
+        return own.order_by(Post.timestamp.desc())
+    
+    def blog_posts(self):
+        return Post.query.order_by(Post.timestamp.desc())
+    
     #this function is to let user can follow another user
     def follow(self, user):
         if not self.is_following(user): 
@@ -58,42 +45,35 @@ class User(db.Model, UserMixin):
         if self.is_following(user): 
             self.followed.remove(user) #this function remove() is called for the unfollow function
             return self 
-    #this is to check who are followed by users
-    
-    def showfollowing(self, user): 
+    #the user contains two elements: followed and followers. Therefore they can check if they follow each other or not
+    def is_following(self, user): #this is to check if a link of two users currently follow each other or not
         return self.followed.filter(
-            self.followers.c.following_id == user.id).count() > 0 #count function is to return number of results      
-    
+            self.followers.c.followed_id == user.id).count() > 0 #count function is to return number of results
+
     def followed_posts(self):
-        followed = Post.query.join(
-            followers, (followers.c.followed_id == Post.user_id)).filter(
-                followers.c.follower_id == self.id)
-        own = Post.query.filter_by(user_id=self.id)
-        return followed.union(own).order_by(Post.timestamp.desc())
-    
+        return Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id).order_by(Post.timestamp.desc())
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+@login.user_loader
+def load_user(id):                                                             # return current user id
+    return User.query.get(int(id))
+
 class Post(db.Model):                                                           # post class model
     id = db.Column(db.Integer, primary_key=True)                                # post ID
     body = db.Column(db.String(140))                                            # main text for the post
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)     # post timestamp
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))                   # user ID that submitted the post
-                       
+    image = db.Column(db.String(20), nullable=True)                             # image file name
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
 
     def __repr__(self):
         return "<Post {}>".format(self.body)
 
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-#this is to extend for the user can send private message to the followed 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id')) #this is for the sender 
-    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id')) #this is for the receiver
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) #this shows the time when user read the message
-
-    def __repr__(self):
-        return '<Message {}>'.format(self.body)
-
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"), nullable=False)
