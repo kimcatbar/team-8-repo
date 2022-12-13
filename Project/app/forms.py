ee0c994579be5b09d674bfb23ea9b89633364d5e
Ngocmai139 <117431455+Ngocmai139@users.noreply.github.com>
2022-12-12 23:08:09 -0800

    conflict update

--- Project/app/forms.py
+++ Project/app/forms.py
@@ -2,31 +2,29 @@ from app.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from flask_wtf.file import FileField, FileAllowed
-from wtforms.validators import DataRequired,Email,EqualTo,InputRequired,Length,ValidationError
-from flask_babel import lazy_gettext #ANTONY CODE
+from wtforms.validators import DataRequired,InputRequired,Length,ValidationError
+from flask_babel import lazy_gettext
 
+class LoginForm(FlaskForm):                                                 # log in form with username, password, remember me, and submit fields
 
-
-
-class LoginForm(FlaskForm):
     username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": lazy_gettext("Username") })
 
     password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": lazy_gettext("Password") })
     
-    remember_me = BooleanField('Remember me')
+    remember_me = BooleanField(lazy_gettext('Remember me'))
 
-    submit = SubmitField('Login')
+    submit = SubmitField(lazy_gettext('Login'))
 
-class RegistrationForm(FlaskForm):
+class RegistrationForm(FlaskForm):                                         # registration form with username, password, and submit fields
     username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": lazy_gettext("Username") })
 
     password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": lazy_gettext("Password")})
 
-    submit = SubmitField('Register')
+    submit = SubmitField(lazy_gettext('Register'))
     
     def validate_username(self, username):                      #if user enters already existing username to signup
         existing_user_username = User.query.filter_by(
@@ -35,13 +33,8 @@ class RegistrationForm(FlaskForm):
             raise ValidationError(
                 'That username already exists. Please choose a different one.')
 
-#this is for the sending a private message 
-class MessageForm(FlaskForm):
-    message = TextAreaField(('Message'), validators=[
-        DataRequired(), Length(min=1, max=500)])
-    submit = SubmitField(('Submit')) #this is to create submit button for the user to send the message 
-#this is to create information button for the user to send the message 
-class EmptyForm(FlaskForm):
-    submit = SubmitField('Submit')   
-
-
+class PostForm(FlaskForm):                                              # post form with post body, image, and submit fields
+    post = TextAreaField(lazy_gettext('Post Something'), validators=[
+        DataRequired(), Length(min=1, max=280)])
+    image = FileField(lazy_gettext('Add image to your post'), validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField(lazy_gettext('Submit'))
