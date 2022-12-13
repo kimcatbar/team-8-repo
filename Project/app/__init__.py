9b9f920145f1a208731dadd16a1ec7152877f3a3
Ngocmai139 <117431455+Ngocmai139@users.noreply.github.com>
2022-12-12 23:07:13 -0800

    manual conflict resolve

--- Project/app/__init__.py
+++ Project/app/__init__.py
@@ -1,31 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 from flask_babel import Babel
 import os
-from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+myapp_obj = Flask(__name__)                                                             # flask app name declaration
 
-myapp_obj = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))                                    # set directory path
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-myapp_obj.config.update(
+myapp_obj.config.update(                                                                # configuration updates
     SECRET_KEY='this-is-a-secret',
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
     SQLALCHEMY_TRACK_MODIFICATIONS = False,
-    POSTS_PER_PAGE = 3
+    LANGUAGES = ['en', 'es']
 )
 
-db = SQLAlchemy(myapp_obj)
+db = SQLAlchemy(myapp_obj)          # set db as database variable for the app
+
+babel = Babel(myapp_obj)            # set babel as flask_babel variable for the app
+
+@babel.localeselector               # tells app which language to set the app based on user locale settings
+def get_locale():
+    return request.accept_languages.best_match(myapp_obj.config['LANGUAGES'])
 
 login = LoginManager(myapp_obj)
-
 
 login.login_view = 'login'
 
 from app import routes, models
 
-with myapp_obj.app_context():
+with myapp_obj.app_context():       # creates database
     db.create_all()
     db.session.commit()
